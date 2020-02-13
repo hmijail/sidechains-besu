14a3f128d277eb8c6ebd69e2fad66e03ecb5900a
Horacio Mijail Anton Quiles <hmijail@gmail.com>
2020-02-13 11:31:26 +1000

    SpotlessApply
    Signed-off-by: Horacio Mijail Anton Quiles <hmijail@gmail.com>

--- acceptance-tests/src/test-support/java/org/hyperledger/besu/tests/acceptance/dsl/AcceptanceTestBase.java
+++ acceptance-tests/src/test-support/java/org/hyperledger/besu/tests/acceptance/dsl/AcceptanceTestBase.java
@@ -52,7 +52,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.junit.rules.TestWatcher;
